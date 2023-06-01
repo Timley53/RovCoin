c8ae74c2ad3c3a2731e1d63cb9b760af5742efe9
Adedokun Timileyin <adedokuntimileyin123@gmail.com>
2023-06-01 09:18:32 +0100

    done

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend:{},
+    screens: {
+      //  'lg':{'max': '1024px'} ,
+       'md': {'max' : '1024px'},
+       'sm': {'max':'640px'},
+       'xs': {'max':'340px'},
+    },
+  },
+  plugins: [],
+}
+
